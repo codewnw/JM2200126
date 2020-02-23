d8a8f9b7cbbcc4d7cf1c163e0ea23978e5daa5cc
codewnw <code.wnw@gmail.com>
2020-02-23 12:24:10 +0530

    Login servlet older way

---
+++ src/main/java/com/jm2200126/servlet/LoginServlet.java
@@ -0,0 +1,27 @@
+package com.jm2200126.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		System.out.println("doPost is running...");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		System.out.println(username + " " + password);
+
+		if (username.equals(password)) {
+			resp.sendRedirect("profile.html");
+		} else {
+			resp.sendRedirect("login.html");
+		}
+	}
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		System.out.println("doGet is running...");
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+	<h2>Hello World!</h2>
+	<a href="login.html">Login</a>
 </body>
 </html>
